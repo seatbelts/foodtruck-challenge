a709379ca46b17fe7436c8d16bfa27803568310a
Ismael Valdez <ismael.valdez@algotive.ai>
2024-07-03 20:15:49 -0600

    added filter functionality for permits and a loading screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FoodTruckService } from './services/foodtruck/foodtruck.service';
-import { Observable } from 'rxjs';
+import { Observable, ReplaySubject, map, takeUntil } from 'rxjs';
 import { FoodtruckInterface } from './interfaces/foodtruck';
 
 @Component({
@@ -8,20 +8,53 @@ import { FoodtruckInterface } from './interfaces/foodtruck';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'foodtruck-challenge';
 
   public foodTruckData$: Observable<FoodtruckInterface[]> | undefined;
+  public foodTruckDataAll$: Observable<FoodtruckInterface[]> | undefined;
+
+  private destroy$: ReplaySubject<boolean> = new ReplaySubject;
 
   constructor(
     private foodTruckService: FoodTruckService
   ) { }
 
-  ngOnInit() {
+   /**
+   *
+   * @function ngOnInit Initilialize component and variables with API data
+   * @returns {void} Void function
+   */
+  ngOnInit(): void {
     this.foodTruckData$ = this.foodTruckService.getTruckinfo();
+    this.foodTruckDataAll$ = this.foodTruckData$;
+  }
 
-    this.foodTruckData$.subscribe(data => {
-      console.log(data)
-    })
+  /**
+   *
+   * @function filteryBy Filter data based on the Permit status given by the selection
+   * @param {permitStatus} A string representing the Permit Status, can only be ALL, REQUESTED, SUSPEND, EXPIRED
+   * @returns {void} Void function
+   */
+  public filteryBy(permitStatus: string): void {
+    if (permitStatus === 'ALL') {
+      this.foodTruckData$ = this.foodTruckDataAll$;
+    } else {
+      
+      this.foodTruckData$ = this.foodTruckDataAll$?.pipe(
+        map((truckData: FoodtruckInterface[]) => truckData.filter((truckInfo: FoodtruckInterface) => truckInfo.permitStatus === permitStatus)),
+        takeUntil(this.destroy$)
+      )
+    }
+  }
+
+  /**
+   *
+   * @function ngOnDestroy Cleans up the subscription of foodTruckDataAll$
+   * @returns {void} Void function
+   */
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
+    this.destroy$.complete();
   }
 }
