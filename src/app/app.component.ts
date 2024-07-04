99583fc36abc46a0db3f55540ba1bcf18ecc5c87
Ismael Valdez <ismael.valdez@algotive.ai>
2024-07-03 19:39:54 -0600

    added cards into component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FoodTruckService } from './services/foodtruck/foodtruck.service';
+import { Observable } from 'rxjs';
+import { FoodtruckInterface } from './interfaces/foodtruck';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'foodtruck-challenge';
+
+  public foodTruckData$: Observable<FoodtruckInterface[]> | undefined;
+
+  constructor(
+    private foodTruckService: FoodTruckService
+  ) { }
+
+  ngOnInit() {
+    this.foodTruckData$ = this.foodTruckService.getTruckinfo();
+
+    this.foodTruckData$.subscribe(data => {
+      console.log(data)
+    })
+  }
 }
